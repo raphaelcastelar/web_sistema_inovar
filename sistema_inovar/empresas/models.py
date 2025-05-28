869446ebf94b6c3e0ec838e72af7e95918114b94
Raphael Castelar <castelaraphael@gmail.com>
2025-05-28 10:04:15 -0300

    att

--- sistema_inovar/empresas/models.py
+++ sistema_inovar/empresas/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Empresa(models.Model):
+    nome = models.CharField(max_length=100)
+    cnpj = models.CharField(max_length=14, unique=True)
+    email = models.EmailField()
+    telefone = models.CharField(max_length=15, blank=True)
+    flags = models.JSONField(default=list)
+
+    def __str__(self):
+        return self.nome
--- sistema_inovar/empresas/views.py
+++ sistema_inovar/empresas/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Empresa
+from .serializers import EmpresaSerializer
 
-# Create your views here.
+class EmpresaViewSet(viewsets.ModelViewSet):
+    queryset = Empresa.objects.all()
+    serializer_class = EmpresaSerializer
+    filterset_fields = ['nome', 'cnpj', 'email', 'telefone']
