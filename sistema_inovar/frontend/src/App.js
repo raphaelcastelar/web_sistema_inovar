a5131c099c9fdc36ec36502e8189848da4ada525
Raphael Castelar <castelaraphael@gmail.com>
2025-06-02 11:35:42 -0300

    run on mac (cache)

--- sistema_inovar/frontend/src/App.js
+++ sistema_inovar/frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import Navbar from './components/navbar';
 import EmpresaList from './components/EmpresaList';
 import EmpresaForm from './components/EmpresaForm';
 import PastaManager from './components/PastaManager';
