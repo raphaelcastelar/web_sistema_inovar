d14c044490fd7064a9fcc4d89c78a984244c858e
Raphael Castelar <castelaraphael@gmail.com>
2025-06-03 15:40:54 -0300

    Update EmpresaForm.js

--- sistema_inovar/frontend/src/components/EmpresaForm.js
+++ sistema_inovar/frontend/src/components/EmpresaForm.js
@@ -51,7 +51,7 @@ const EmpresaForm = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     setLoading(true);
-    const url = empresaId ? `http://192.168.196.162:8000/api/empresas/${empresaId}/` : `http://127.0.0.1:8000/api/empresas/`;
+    const url = empresaId ? `http://192.168.196.162:8000/api/empresas/${empresaId}/` : `http://192.168.196.162:8000/api/empresas/`;
     const method = empresaId ? 'put' : 'post';
 
     axios[method](url, empresa)
