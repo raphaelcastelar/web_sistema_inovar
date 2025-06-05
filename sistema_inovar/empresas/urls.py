37fe300869d6ce425b661f3f2159183573d6d078
Raphael Castelar <castelaraphael@gmail.com>
2025-06-05 13:43:08 -0300

    Update urls.py

--- sistema_inovar/empresas/urls.py
+++ sistema_inovar/empresas/urls.py
@@ -2,9 +2,11 @@
 from django.urls import path
 from .views import (
     enviar_email, 
-    enviar_documentos_whatsapp_api
+    enviar_documentos_whatsapp_api,
+    sincronizar_pasta_empresa_api
 )
 urlpatterns = [
     path('enviar-email/', enviar_email, name='enviar_email'),
     path('enviar-documentos-whatsapp/', enviar_documentos_whatsapp_api, name='enviar_documentos_whatsapp'),
+    path('sincronizar-pasta/', sincronizar_pasta_empresa_api, name='sincronizar_pasta_empresa'),
 ]
